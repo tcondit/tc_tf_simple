335b7ea68704fbefb6ee649bb5cc0c3ddf1204cb
Tim Condit (izz731) <TimCondit@capitalone.com>
2016-11-17 17:26:49 -0800

    Initial commit, tiny stack config
    Also the .tfstate file from the first run. tfstate files may not work
    out great in git, but it's fine for single-user mode (and practice).

---
+++ example.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    access_key = "AKIAJTCJJCNRMEPUG7QQ"
+    secret_key = "ZqjB7ag48PeJaX6YnUWhzv2TMRWE13qsSH8WCEPV"
+    region     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+    count         = 2
+    ami           = "ami-408c7f28"
+    instance_type = "t1.micro"
+    tags {
+        name      = "${id}"
+    }
+}
