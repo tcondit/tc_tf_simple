9eac57d7c16eb706a73df29005d33a956032f341
Tim Condit (izz731) <TimCondit@capitalone.com>
2016-11-21 16:21:55 -0800

    update to t2.micro (using default VPC)

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aws" {
 resource "aws_instance" "example" {
     count          = 1
     ami            = "${lookup(var.amis, var.region)}"
-    instance_type  = "t1.micro"
+    instance_type  = "t2.micro"
 
     provisioner "local-exec" {
         command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
--- variables.tf
+++ variables.tf
@@ -9,7 +9,7 @@ variable "amis" {
     type = "map"
     default = {
         /* https://cloud-images.ubuntu.com/releases/14.04/release-20150408.1/ */
-        us-east-1 = "ami-408c7f28"
-        us-west-2 = "ami-595b7069"
+        us-east-1 = "ami-ebb185fc"
+        us-west-2 = "ami-e7a50987"
     }
 }
